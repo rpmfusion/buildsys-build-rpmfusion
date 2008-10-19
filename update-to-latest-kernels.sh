6e2c27ec235940fc4c68549555ae0b4a8c7dc3db
Thorsten Leemhuis <fedora@leemhuis.info>
2008-10-19 10:55:53 +0000

    add update helper script

---
+++ update-to-latest-kernels.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+print_kernellist()
+{
+	echo ${1}
+	echo ${1}smp
+	echo ${1}PAE
+}
+
+if [[ ! "${1}" ]] ; then
+	echo "Please call with verrel of latest standard kernel version" >&2
+	exit 1
+fi
+
+if [[ ! "${RPM_PACKAGER}" ]] ; then
+	echo "Please set RPM_PACKAGER for rpmdev-bumpspec" >&2
+	exit 1
+fi
+
+# update spec file
+rpmdev-bumpspec -c "- rebuild for kernel ${1}" *.spec
+# update buildsys-build-rpmfusion-kerneldevpkgs-current
+print_kernellist ${1} > buildsys-build-rpmfusion-kerneldevpkgs-current
+
+cvs diff -u
+read
+make clog; cvs commit -F clog 
+rm clog
+make tag build
